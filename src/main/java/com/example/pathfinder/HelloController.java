3d1d5b702db5fbd9dc611f290b1e224565734a3f
Esperlluette <janl.mannessier@gmail.com>
2022-12-02 09:16:41 +0100

    first commit

---
+++ src/main/java/com/example/pathfinder/HelloController.java
@@ -0,0 +1,9 @@
+package com.example.pathfinder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+// Class controller home page, add ui component here.
+
+}
